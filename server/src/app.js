e849edba399947db2b8603c4c91330dbdbd14519
official-Tushar <tusharsrivastava8404@gmail.com>
2025-07-02 13:01:47 +0530

    Allowed cors for the Deployed frontned

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+        "https://share-nest-zeta.vercel.app"
+    ],
     credentials: true,
 }));
 app.use(express.json());
